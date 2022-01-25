fbd0558b138a018efcd25c5bf16668980c54bb5a
matjag102 <58480022+matjag102@users.noreply.github.com>
2022-01-25 19:13:15 +0100

    Add files via upload

--- strona/glowna.php
+++ strona/glowna.php
@@ -5,21 +5,6 @@
         header('Location: index.php');
         exit();
     }
-    if($_SESSION['rola']==2)
-    {
-        header('Location: glowna_trener.php');
-        exit();
-    }
-    elseif($_SESSION['rola']==3)
-    {
-        header('Location: glowna_organizator.php');
-        exit();
-    }
-    elseif($_SESSION['rola']==4)
-    {
-        header('Location: glowna_admin.php');
-        exit();
-    }
 
 ?>
 <!DOCTYPE html>
@@ -41,12 +26,12 @@
 <body>
     <div id="container">
         <div id="header">
-            <div id="logo"><a href="index.php">logo</a><br>placeholder</div>
+            <div id="logo"><a href="glowna.php"><img src=pies.png alt="pies logo"></a></div>
             <div id="nav">
                 <button id="button"><a href="faq.php">FAQ</a></button>
                 <button id="button"><a href="galeria.php">Galeria</a></button>
                 <button id="button"><a href="transmisja.php">Transmisja na żywo</a></button>
-                <button id="button"><a href="zaglosuj.php">Zagłosuj</a></button>
+                <button id="button"><a href="#">Zagłosuj</a></button>
                 <button id="button"><a href="harmonogram.php">Harmonogram wystaw</a></button>
                 <button id="button"><a href="aktualnosci.php">Aktualności</a></button>
             </div>
@@ -54,7 +39,11 @@
         <div id="content">
             <div id="mid"><b></b>
                 <div id="mid-header">Aktualności</div>
-                <div id="mid-content">placeholder</div>
+                <div id="mid-content">
+                    <?php
+                        require_once("aktualnosci_display.php");
+                    ?>
+                </div>
             </div>
             <div id="right-bar">
                 <div id="right-bar-header">
@@ -63,9 +52,56 @@
                     ?></div>
                 <div id="right-bar-content1"><button id="button1"><a href="wyloguj.php">Wyloguj się</a></button></div>
                 <div id="right-bar-content2">
+                  <?php
+                        switch($_SESSION['rola']){
+                            case 1:
+                                    echo "Rola: Użytkownik";
+                            break;
+                                
+                            case 2:
+                                    echo "Rola: Trener";
+                            break;
+                                
+                            case 3:
+                                    echo "Rola: Organizator";
+                            break;
+                            
+                            case 4:
+                                    echo '<font color="red">Rola: Operator</font>';
+                            break;
+                        }
+                    ?>
                 </div>
-                <div id="right-bar-content3"></div>
-                <div id="right-bar-content4"></div>
+                <div id="right-bar-content3">
+                     <?php
+                        switch($_SESSION['rola']){
+                            case 1:
+                                
+                            break;
+                                
+                            case 2:
+                                    echo "Panel trenera: <br>";
+                                    echo '<a href="szkolenie_org.php" class="panel-item">Organizowanie szkolenia</a><br>';
+                            break;
+                                
+                            case 3:
+                                    echo "Panel organizatora: <br>";
+                                    echo '<a href="wystawa_org.php" class="panel-item">Organizowanie wystawy</a><br>';
+                                    echo '<a href="wystawa_zarzadzaj.php" class="panel-item">Zarzadzaj wystawami</a><br>';
+                                    echo '<a href="wstaw_wpis.php" class="panel-item">Wstawianie-Aktualności</a><br>';
+                                    echo '<a href="modyfikuj_wpis.php" class="panel-item">Zarzadzaj-Aktualności</a><br>';
+                                
+                            break;
+                            
+                            case 4:
+                                    echo 'Panel operatora: <br>';
+                                    echo '<a href="zgloszenia_crud.php" class="panel-item">Zgłoszenia</a><br>';
+                                    echo '<a href="konta_crud.php" class="panel-item">Zgłoszenia</a><br>';
+                            break;
+                        }
+                    ?>
+                  
+                </div>
             </div>
         </div>
         <div id="footer"></div>
